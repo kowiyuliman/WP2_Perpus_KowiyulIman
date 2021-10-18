e58d7532476d640ad1535239c5510983f4e510b1
kowiyuliman <kowiyuliman17@gmail.com>
2021-10-18 23:01:03 +0700

    tugas pertemuan-6

---
+++ application/controllers/contoh.php
@@ -0,0 +1,13 @@
+<?php 
+class contoh extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo " Nama saya Kowiyul Iman <br>
+            saya adalah mahasiswa aktif di UBSI Pemuda semester 3<br>
+            dan saya tinggal di daerah bekasi";
+    }
+}
+
+?>  
